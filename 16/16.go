70b8b1370f05c0291558409b3caff97de54b7218
Eric Fung <eric.fung@shopify.com>
2016-01-29 10:07:07 -0500

    More debugging

--- 16/16.go
+++ 16/16.go
@@ -44,77 +44,88 @@ func main() {
 type Sue struct {
 	id int
 	// -1 means don't know; non-negative is known value
-	children, cats, samoyeds, pomeranians, akitas, vizslas, goldfish, trees, cars, perfumes int
+	CHILDREN, CATS, SAMOYEDS, POMERANIANS, AKITAS, VIZSLAS, GOLDFISH, TREES, CARS, PERFUMES int
 }
 
 func GiftSue() Sue {
-	s := NewSue(0)
-	s.SetProperty("children", 3)
-	s.SetProperty("cats", 7)
-	s.SetProperty("samoyeds", 2)
-	s.SetProperty("pomeranians", 3)
-	s.SetProperty("akitas", 0)
-	s.SetProperty("vizslas", 0)
-	s.SetProperty("goldfish", 0)
-	s.SetProperty("trees", 0)
-	s.SetProperty("cars", 2)
-	s.SetProperty("perfumes", 1)
+	var s Sue = Sue{}
+	s.id = 0
+	s.SetProperty("CHILDREN", 3)
+	s.SetProperty("CATS", 7)
+	s.SetProperty("SAMOYEDS", 2)
+	s.SetProperty("POMERANIANS", 3)
+	s.SetProperty("AKITAS", 0)
+	s.SetProperty("VIZSLAS", 0)
+	s.SetProperty("GOLDFISH", 5)
+	s.SetProperty("TREES", 3)
+	s.SetProperty("CARS", 2)
+	s.SetProperty("PERFUMES", 1)
 	return s
 }
 
 func NewSue(id int) Sue {
 	var s Sue = Sue{}
 	s.id = id
-	s.SetProperty("children", -1)
-	s.SetProperty("cats", -1)
-	s.SetProperty("samoyeds", -1)
-	s.SetProperty("pomeranians", -1)
-	s.SetProperty("akitas", -1)
-	s.SetProperty("vizslas", -1)
-	s.SetProperty("goldfish", -1)
-	s.SetProperty("trees", -1)
-	s.SetProperty("cars", -1)
-	s.SetProperty("perfumes", -1)
+	s.SetProperty("CHILDREN", -1)
+	s.SetProperty("CATS", -1)
+	s.SetProperty("SAMOYEDS", -1)
+	s.SetProperty("POMERANIANS", -1)
+	s.SetProperty("AKITAS", -1)
+	s.SetProperty("VIZSLAS", -1)
+	s.SetProperty("GOLDFISH", -1)
+	s.SetProperty("TREES", -1)
+	s.SetProperty("CARS", -1)
+	s.SetProperty("PERFUMES", -1)
 	return s
 }
 
-func (aSue Sue) SetProperty(property string, value int) {
-	ps := reflect.ValueOf(&aSue).Elem()
+func (aSue *Sue) SetProperty(property string, value int) {
+	ps := reflect.ValueOf(aSue).Elem()
 	p := ps.FieldByName(property)
-	//if p.IsValid() && p.CanSet() {
-	p.SetInt(int64(value))
-	//	}
+	if p.IsValid() && p.CanSet() {
+		p.SetInt(int64(value))
+	}
 }
 
 func (this Sue) Compare(that Sue) bool {
-	if this.children != -1 && that.children != -1 && this.children != that.children {
+	if this.CHILDREN != -1 && that.CHILDREN != -1 && this.CHILDREN != that.CHILDREN {
+		fmt.Println("Children mismatch")
 		return false
 	}
-	if this.cats != -1 && that.cats != -1 && this.cats != that.cats {
+	if this.CATS != -1 && that.CATS != -1 && this.CATS != that.CATS {
+		fmt.Println("cats mismatch")
 		return false
 	}
-	if this.samoyeds != -1 && that.samoyeds != -1 && this.samoyeds != that.samoyeds {
+	if this.SAMOYEDS != -1 && that.SAMOYEDS != -1 && this.SAMOYEDS != that.SAMOYEDS {
+		fmt.Println("sam mismatch")
 		return false
 	}
-	if this.pomeranians != -1 && that.pomeranians != -1 && this.pomeranians != that.pomeranians {
+	if this.POMERANIANS != -1 && that.POMERANIANS != -1 && this.POMERANIANS != that.POMERANIANS {
+		fmt.Println("pom mismatch")
 		return false
 	}
-	if this.akitas != -1 && that.akitas != -1 && this.akitas != that.akitas {
+	if this.AKITAS != -1 && that.AKITAS != -1 && this.AKITAS != that.AKITAS {
+		fmt.Println("aki mismatch")
 		return false
 	}
-	if this.vizslas != -1 && that.vizslas != -1 && this.vizslas != that.vizslas {
+	if this.VIZSLAS != -1 && that.VIZSLAS != -1 && this.VIZSLAS != that.VIZSLAS {
+		fmt.Println("viz mismatch")
 		return false
 	}
-	if this.goldfish != -1 && that.goldfish != -1 && this.goldfish != that.goldfish {
+	if this.GOLDFISH != -1 && that.GOLDFISH != -1 && this.GOLDFISH != that.GOLDFISH {
+		fmt.Println("gf mismatch")
 		return false
 	}
-	if this.trees != -1 && that.trees != -1 && this.trees != that.trees {
+	if this.TREES != -1 && that.TREES != -1 && this.TREES != that.TREES {
+		fmt.Println("trees mismatch")
 		return false
 	}
-	if this.cars != -1 && that.cars != -1 && this.cars != that.cars {
+	if this.CARS != -1 && that.CARS != -1 && this.CARS != that.CARS {
+		fmt.Println("cars mismatch")
 		return false
 	}
-	if this.perfumes != -1 && that.perfumes != -1 && this.perfumes != that.perfumes {
+	if this.PERFUMES != -1 && that.PERFUMES != -1 && this.PERFUMES != that.PERFUMES {
+		fmt.Println("perf mismatch")
 		return false
 	}
 	return true
